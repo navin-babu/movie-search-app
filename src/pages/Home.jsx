4beb624cadf6523858bf377219ae74809ee60444
Navin Babu <navinbabu05@gmail.com>
2026-03-04 17:49:34 +0530

    Search added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,47 @@
 import React, { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
-import "../css/Home.css"
+import "../css/Home.css";
 import { searchMovie, getPopularMovies } from "../services/api";
 
 function Home() {
-
   const [searchQuery, setSearchQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
- 
-  useEffect(()=> {
-    const loadPopularMovies = async() => {
-        try {
-            const popularMovies = await getPopularMovies()
-            setMovies(popularMovies)
-        } catch(err) {
-            console.log(err);
-            setError("Failed to load movies....")
-        } finally {
-            setLoading(false)
-        }
-    }
-    loadPopularMovies()
-  }, [])
 
-  const handleSearch = (e) => {
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load movies....");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
+
+  const handleSearch = async(e) => {
     e.preventDefault();
-    alert(searchQuery);
+    if (!searchQuery.trim()) return
+    if (loading) return
+
+    setLoading(true)
+
+    try {
+        const searchResults = await searchMovie(searchQuery)
+        setMovies(searchResults)
+        setError(null)
+
+    } catch(err) {
+        console.log(err)
+    } finally {
+        setLoading(false)
+    }
+
   };
 
   return (
@@ -38,16 +52,24 @@ function Home() {
           placeholder="Search for movies..."
           className="search-input"
           value={searchQuery}
-          onChange={(e)=> setSearchQuery(e.target.value)}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <button type="submit" className="search-button">Search</button>
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </form>
 
-      <div className="movies-grid">
-        {movies.map((movie) => ( 
+      {error && <div className="error-message">{error}</div> }
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
             <MovieCard movie={movie} key={movie.id} />
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
